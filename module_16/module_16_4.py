de63f03f6c3368886470f2e38e49a95a905820f3
arturlarcev2001 <45388557+arturlarcev2001@users.noreply.github.com>
2024-12-04 19:34:46 +0300

    Update module_16_4.py

--- module_16/module_16_4.py
+++ module_16/module_16_4.py
@@ -37,7 +37,7 @@ async def create_user(
     if len(users) == 0:
         id = 1
     else:
-        id = len(users) + 1
+        id = users[len(users)-1].id + 1
     user = User(id=id, name=username, age=age)
     users.append(user)
     return f'User {id} was created'
